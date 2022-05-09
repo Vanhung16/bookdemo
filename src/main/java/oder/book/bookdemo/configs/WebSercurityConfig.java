1514d5cafb9f617ae6de11a23679f7c14731a987
Hunguyen <hungnguyenvan209@gmail.com>
2022-05-09 23:24:02 +0700

    ...

--- src/main/java/oder/book/bookdemo/configs/WebSercurityConfig.java
+++ src/main/java/oder/book/bookdemo/configs/WebSercurityConfig.java
@@ -45,6 +45,5 @@ public class WebSercurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
-
     }
 }
--- src/main/java/oder/book/bookdemo/controllers/AuthController.java
+++ src/main/java/oder/book/bookdemo/controllers/AuthController.java
@@ -1,2 +1,13 @@
-package oder.book.bookdemo.controllers;public class AuthController {
+package oder.book.bookdemo.controllers;
+
+import oder.book.bookdemo.Service.Impl.UserDetailServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class AuthController {
+    @Autowired
+    UserDetailServiceImpl userDetailService;
 }
